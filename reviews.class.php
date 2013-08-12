7c4e34b693f0e83186e09189a2bb04913062ce2a
sekret47 <dima@mediagroup.com.ua>
2013-08-12 23:31:43 +0300

    Update reviews.class.php

--- reviews.class.php
+++ reviews.class.php
@@ -1,11 +1,21 @@
 <?php
 class Reviews{
+
 public $link;
 public $error;
 
+
+   protected  function db_connect(){
+        $link = mysqli_connect('localhost','root','','resume');
+        return $link;
+    }
+
     function get_news(){
+        $link   = $this->db_connect();
         $sql    = "SELECT id, title, create_date, user, text FROM news ORDER BY id DESC";
-        $query  = mysqli_query($this->link,$sql);
+
+        //mysqli_query ($link,"SET NAMES utf8");
+        $query  = mysqli_query($link,$sql);
 
         if(mysqli_num_rows($query) >=1){
             for($i = 0;$i < mysqli_num_rows($query);$i++){
@@ -15,16 +25,23 @@ public $error;
         }
     }
 
- function set_news($date,$user,$news_text){
-     if($news_text){
-         $sql = "INSERT INTO news (create_date,user,text) VALUES ('$date','$user','$news_text')";
-         mysqli_query($this->link,$sql) or die($error = mysqli_error($this->link));
-         header('Location: index.php');
-     }else{
-         header('Location: index.php');
-
+     function validate($post){
+        return nl2br(strip_tags($post));
      }
 
-   }
+     function set_news($date,$user,$news_text){
+         $link = $this->db_connect();
+         $this->validate($user);
+         $this->validate($news_text);
+
+         if($news_text){
+             $sql = "INSERT INTO news (create_date,user,text) VALUES ('$date','$user','$news_text')";
+             mysqli_query($link,$sql) or die($error = mysqli_error($this->link));
+             header('Location: index.php');
+         }else{
+             header('Location: index.php');
+         }
+
+       }
 
 }
