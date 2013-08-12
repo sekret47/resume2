6fb47885668deda78368a4ffbb762d937cc3b0e9
sekret47 <dima@mediagroup.com.ua>
2013-08-12 23:32:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,26 @@
-<meta charset="utf-8">
 <?php
 include('reviews.class.php');
 include ("libs/Smarty.class.php");
-$read = file('resume.txt');
+
+//Create Smarty Object
 $smarty = new Smarty();
 
+$read = file('resume.txt');
 $smarty->assign('resume',$read);
 
-$db   = new Reviews();
-$link = $db->link = mysqli_connect('sevcomfo.mysql.ukraine.com.ua','sevcomfo_db','M59xjs3d','sevcomfo_db');
-
-mysqli_query ($link,"SET NAMES utf8");
+$db  = new Reviews();
 $news_array = $db->get_news();
-
-if(isset($_POST['submit'])){
-    $news_text = nl2br(strip_tags($_POST['news']));
-    $user      = strip_tags($_POST['user']);
-    $date      = date('H:i:s m.d.y');
-    $db->set_news($date,$user,$news_text);
-}
-
-if(isset($db->error)){
-    foreach($db->error as $error){
-        echo $error;
-    }
-}
 
 if($news_array){
     $smarty->assign('news',$news_array);
+}
+
+if(isset($_POST['submit'])){
+    $news_text = $db->validate($_POST['news']);
+    $user      = $db->validate($_POST['user']);
+    $date      = date('H:i:s m.d.y');
+
+    $db->set_news($date,$user,$news_text);
 }
 
 $smarty->display('index.tpl');
